301842675af3988f373c8f1c5c9cf75a7e5f4ea4
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-04-13 22:32:37 +0300

    (TP-66) fix: fix categories

--- rentplace_frontend/src/pages/FavoritesPage.js
+++ rentplace_frontend/src/pages/FavoritesPage.js
@@ -2,18 +2,28 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 import favoritesStore from "../store/favoritesStore";
 import PropertyCard from "../components/PropertyCard";
-import "./FavoritesPage.css"
+import Header from "../components/Header";
+import Categories from "../components/Categories";
+import RecentFirst from "../components/RecentFirst";
+import PropertiesList from "../components/PropertiesList";
+import "./FavoritesPage.css";
 
 const FavoritesPage = () => {
+  const isEmpty = favoritesStore.favorites.length === 0;
   return (
-    <div className="favorires-container">
-      {favoritesStore.favorites.length > 0 ? (
-        favoritesStore.favorites.map((property) => (
-          <PropertyCard key={property.id} property={property} />
-        ))
-      ) : (
-        <p>В избранном пока ничего нет.</p>
-      )}
+    <div className="favorites-page">
+      <Header></Header>
+      <RecentFirst></RecentFirst>
+      <Categories></Categories>
+      <div className={`cards_container ${isEmpty ? "empty" : ""}`}>
+        {favoritesStore.favorites.length > 0 ? (
+          favoritesStore.favorites.map((property) => (
+            <PropertyCard key={property.id} property={property} />
+          ))
+        ) : (
+          <p>У вас нет избранных объявлений.</p>
+        )}
+      </div>
     </div>
   );
 };
