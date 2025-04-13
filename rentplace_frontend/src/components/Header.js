1fcf79725045dab0897492d7281093508df9ba9a
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-04-13 22:40:31 +0300

    (TP-66) fix: fix navigation changing title

--- rentplace_frontend/src/components/Header.js
+++ rentplace_frontend/src/components/Header.js
@@ -1,16 +1,38 @@
 import React from "react";
+import { useLocation } from "react-router-dom"; // Импортируем хук для определения текущего маршрута
 import "./Header.css";
 
 const Header = () => {
+  const location = useLocation(); // Получаем текущий маршрут
+
+  // Определяем текст в зависимости от маршрута
+  const getHeaderText = () => {
+    switch (location.pathname) {
+      case "/":
+        return "rentplace"; // Текст для главной страницы
+      case "/favorites":
+        return "Избранное"; // Текст для страницы избранного
+      case "/bookings":
+        return "Мои брони"; // Текст для страницы бронирований
+      case "/create":
+        return "Сдать жилье"; // Текст для страницы создания объявления
+      case "/profile":
+        return "Профиль"; // Текст для страницы профиля
+      default:
+        return "Rentplace"; // Дефолтный текст
+    }
+  };
+
   return (
-      <div className="upper_header">
-        <div className="logo">
-          <img src="./images/logo.png" alt="Rentplace Logo" />
-        </div>
-        <div className="profile">
-          <img src="./images/profile.png" alt="Profile" />
-        </div>
+    <div className="upper_header">
+      <div className="logo">
+        <img src="./images/logo.png" alt="Rentplace Logo" />
+        <span className="header-text">{getHeaderText()}</span> {/* Добавляем текст */}
       </div>
+      <div className="profile">
+        <img src="./images/profile.png" alt="Profile" />
+      </div>
+    </div>
   );
 };
 
