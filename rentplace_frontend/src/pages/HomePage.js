2cc46f120356ca7faeb7327af74f3cbdc79cce95
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-04-13 22:35:15 +0300

    (TP-66) fix: fix layout

--- rentplace_frontend/src/pages/HomePage.js
+++ rentplace_frontend/src/pages/HomePage.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PropertiesList from "../components/PropertiesList";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-container">
+      <Header></Header>   
+      <SearchBar></SearchBar>
+      <Categories></Categories>
       <PropertiesList />
     </div>
   );
