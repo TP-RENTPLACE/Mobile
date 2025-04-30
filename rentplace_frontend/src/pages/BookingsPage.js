dbc79a8881def382cddb0361a2671dc65884e232
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-04-30 18:58:52 +0300

    (TP-66) fix: minor fixes

--- rentplace_frontend/src/pages/BookingsPage.js
+++ rentplace_frontend/src/pages/BookingsPage.js
@@ -3,17 +3,21 @@ import Header from "../components/Header";
 import RecentFirst from "../components/RecentFirst";
 import Categories from "../components/Categories";
 import "./BookingsPage.css";
+import BottomNavigation from "../components/BottomNavigation";
 
 const BookingsPage = () => {
   return (
-    <div className="booking">
-      <Header></Header>
-      <RecentFirst></RecentFirst>
-      <Categories></Categories>
-      <div className="cards_container">
-        <p>У вас нет активных броней</p>
+    <>
+      <div className="booking">
+        <Header></Header>
+        <RecentFirst></RecentFirst>
+        <Categories></Categories>
+        <div className="cards_container">
+          <p>У вас нет активных броней</p>
+        </div>
       </div>
-    </div>
+      <BottomNavigation />
+    </>
   );
 };
 
