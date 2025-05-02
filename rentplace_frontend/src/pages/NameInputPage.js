376a5f3d30335013e8cb5c2028ab1f7148c1050b
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-02 17:32:50 +0300

    (TP-66) fix: name input section

--- rentplace_frontend/src/pages/NameInputPage.js
+++ rentplace_frontend/src/pages/NameInputPage.js
@@ -1,19 +1,31 @@
 // pages/NameInputPage.js
-import React from 'react';
+import React from "react";
+import HeadWithText from "../components/HeadWithText";
+import BigBlueButton from "../components/BigBlueButton";
+import "./NameInputPage.css";
 
 const NameInputPage = () => {
   const handleComplete = () => {
     // Здесь можно добавить логику завершения регистрации
-    alert('Регистрация завершена!');
+    alert("Регистрация завершена!");
   };
 
   return (
-    <div className="auth-page">
-      <h1>Введите ваше имя и фамилию</h1>
-      <input type="text" placeholder="Имя" />
-      <input type="text" placeholder="Фамилия" />
-      <button onClick={handleComplete}>Завершить</button>
-    </div>
+    <>
+      {" "}
+      <div className="auth-page">
+        <HeadWithText props="Вход/Регистрация" />
+        <div className="auth-page_body">
+          <h1>Укажите имя и фамилию</h1>
+          <span>Имя</span>
+          <input type="email" placeholder="Имя" />
+          <span>Фамилия</span>
+          <input type="email" placeholder="Фамилия" />
+          <BigBlueButton onClick={handleComplete} props="Далее"/>
+          
+        </div>
+      </div>
+    </>
   );
 };
 
