773cbf3f151fca2a558196ebc894f5a73a580c4b
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-05 21:21:04 +0300

    (TP-66) fix: arrow-back fix

--- rentplace_frontend/src/components/HeadWithText.js
+++ rentplace_frontend/src/components/HeadWithText.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./HeadWithText.css";
 import { ArrowLeft } from "lucide-react";
+import { useNavigate } from 'react-router-dom';
 
 const HeadWithText = ({props}) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="head-with-text">
-        <ArrowLeft className="icon" />
+        <ArrowLeft className="icon" onClick={() => navigate(-1)}/>
         <span>{props}</span>
       </div>
       <div className="divider"></div>
