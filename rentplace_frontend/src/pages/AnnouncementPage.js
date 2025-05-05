d21d98629b0ce5c4a0a7303daf29759eba3d9897
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-05 21:13:05 +0300

    (TP-66) fix: buttons fix

--- rentplace_frontend/src/pages/AnnouncementPage.js
+++ rentplace_frontend/src/pages/AnnouncementPage.js
@@ -1,43 +1,54 @@
-import React from "react";
-import ImageSlider from "../components/ImageSlider";
-import PropertyDetails from "../components/PropertyDetails";
-import Description from "../components/Description";
-import announcements from "../store/data"; // Импортируем массив объявлений
-import "./AnnouncementPage.css";
-import Conveniences from "../components/Сonveniences";
-import HeadWithText from "../components/HeadWithText";
-import BigBlueButton from "../components/BigBlueButton";
-import BottomNavigation from "../components/BottomNavigation";
+import React from 'react';
+import ImageSlider from '../components/ImageSlider';
+import PropertyDetails from '../components/PropertyDetails';
+import Description from '../components/Description';
+import announcements from '../store/data'; // Импортируем массив объявлений
+import "./AnnouncementPage.css"
+import Facilities from '../components/Facilities';
+import HeadWithText from '../components/HeadWithText';
+import BigBlueButton from '../components/BigBlueButton';
+import BottomNavigation from '../components/BottomNavigation';
+import {useLocation, useNavigate} from "react-router-dom";
 import { MapPin } from "lucide-react";
 
+
 const AnnouncementPage = () => {
-  const announcement = announcements[0];
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {property} = location.state || {};
 
-  if (!announcement) {
-    return <div>Объявление не найдено</div>;
-  }
 
-  return (
-    <div className="announcement-page">
-      <HeadWithText props="Объявление" />
-      <ImageSlider />
-      <h1>{announcement.title}</h1>
+    if (!property) {
+        return <div>Объявление не найдено</div>;
+    }
 
-      <PropertyDetails details={announcement.details} />
-      <Description description={announcement.description} />
-      <div className="location">
-        <h3>Расположение</h3>
-        <div className="cont">
-          <MapPin className="icon" />
-          <span>{announcement.location}</span>
+    const handleBookClick = () => {
+        navigate("/booking-form", { state: { property } }); // ← передаём property в booking-form
+    };
+
+    return (
+        <div className="announcement-page">
+            <HeadWithText props="Объявление"/>
+            <ImageSlider images={property.imagesDTOs}/>
+            <h1>{property.title}</h1>
+
+            <PropertyDetails property={property}/>
+
+            <Description property={property}/>
+
+            <div className="location">
+                <h3>Расположение</h3>
+                <div className="cont">
+                    <MapPin className="icon"/>
+                    <span>{property.address}</span>
+                </div>
+            </div>
+
+            <Facilities facilities={property.facilitiesDTOs}/>
+            <BigBlueButton props="Выбрать даты" fix="fixed" onClick={handleBookClick}/>
+            <BottomNavigation/>
         </div>
-      </div>
-      <Conveniences conveniences={announcement.conveniences} />
-
-      <BigBlueButton props="Выбрать даты" fix="fixed" />
-      <BottomNavigation />
-    </div>
-  );
+    );
 };
 
 export default AnnouncementPage;
