1337eee481ee6e5dde2cceaa088ab8fbfa7709d1
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-19 20:28:32 +0300

    (TP-108) feat: added declensions

--- rentplace_frontend/src/components/PropertyDetails.js
+++ rentplace_frontend/src/components/PropertyDetails.js
@@ -1,21 +1,21 @@
-// components/PropertyDetails.js
 import React from 'react';
 import './PropertyDetails.css';
+import {declension} from "../store/declension";
 
 const PropertyDetails = ({ property }) => {
-  if (!property) {
-    return <div>Детали недоступны</div>;
-  }
+    if (!property) {
+        return <div>Детали недоступны</div>;
+    }
 
-  return (
-    <div className="propertyDetails">
-      <span>{property.area} м²</span>
-      <span>{property.rooms} комнат</span>
-      <span>{property.maxGuests} гостей</span>
-      <span>{property.bedrooms} спален</span>
-      <span>{property.sleepingPlaces} кроватей</span>
-    </div>
-  );
+    return (
+        <div className="propertyDetails">
+            <span>{property.area} м²</span>
+            <span>{property.rooms} {declension(property.rooms, ['комната', 'комнаты', 'комнат'])}</span>
+            <span>{property.maxGuests} {declension(property.maxGuests, ['гость', 'гостя', 'гостей'])}</span>
+            <span>{property.bedrooms} {declension(property.bedrooms, ['спальня', 'спальни', 'спален'])}</span>
+            <span>{property.sleepingPlaces} {declension(property.sleepingPlaces, ['кровать', 'кровати', 'кроватей'])}</span>
+        </div>
+    );
 };
 
 export default PropertyDetails;
