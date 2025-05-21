271fdd47448d2cf6437f3f4f3d19937796cd5910
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-21 23:11:26 +0300

    fix: loading screen logo display

--- rentplace_frontend/src/components/Logo.js
+++ rentplace_frontend/src/components/Logo.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from "react";
 import "./Logo.css";
 
-const Logo = ({onComplete}) => {
+const Logo = ({ onComplete }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
-        onComplete();
+      onComplete();
     }, 2000);
     return () => clearTimeout(timer);
-}, [onComplete]);
+  }, [onComplete]);
   return (
-    
     <div className="loading-screen_logo">
       <img src="/icons/logo-white.svg" alt="Rentplace Logo" />
       <span className="logo-text">rentplace</span>
