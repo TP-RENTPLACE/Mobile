bcc38046c770625a98f478922bab2ae19f0e9ae1
ivanpolotskii <ivan.polotskiy30@gmail.com>
2025-05-23 03:19:07 +0300

    (TP-147) feat: display address on the main page

--- rentplace_frontend/src/components/SearchBar.js
+++ rentplace_frontend/src/components/SearchBar.js
@@ -1,44 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchBar.css";
-import { Search } from "lucide-react";
-import { SlidersHorizontal } from "lucide-react";
+import { Search, SlidersHorizontal } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import FiltersPage from "../pages/FiltersPage";
 
 const SearchBar = () => {
   const navigate = useNavigate();
-  const [showFullscreenFilters, setShowFullscreenFilters] = useState(false);
+  const [searchAddress, setSearchAddress] = useState("");
 
-  const handleClickFilters = () => {
-    navigate("/filters");
-  };
+  useEffect(() => {
+    const address = localStorage.getItem("searchAddress");
+    setSearchAddress(address?.trim() || "");
+  }, []);
 
-  const handleCloseFilters = () => {
-    setShowFullscreenFilters(false);
-  };
-
-  const handleClickSearch = () => {
-    navigate("/destination");
-  };
+  const handleClickFilters = () => navigate("/filters");
+  const handleClickSearch = () => navigate("/destination");
 
   return (
-    <>
       <div className="search">
         <div className="search-bar" onClick={handleClickSearch}>
-          <span className="search-icon">
-            <Search />
-          </span>
-          <input 
-            type="text" 
-            placeholder="Куда хотите отправиться?" 
-            readOnly
+        <span className="search-icon">
+          <Search />
+        </span>
+          <input
+              type="text"
+              value={searchAddress || "Куда хотите отправиться?"}
+              readOnly
+              className={searchAddress ? "has-value" : "placeholder"}
           />
         </div>
         <div className="filters-icon" onClick={handleClickFilters}>
           <SlidersHorizontal />
         </div>
       </div>
-    </>
   );
 };
 
